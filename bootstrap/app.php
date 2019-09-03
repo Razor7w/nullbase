7fc96fe60e34795d06bdeb5e8ce70419877522f3
Razor7w <seba.carroza@gmail.com>
2019-09-03 15:34:14 -0400

    [NullBase]
    - Integración de Illuminate/Database

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,11 +8,29 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = new \Slim\App([
     'settings' => [
       'displayErrorDetails' => true,
+      'db' => [
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'database'  => 'nullbase',
+        'username'  => 'root',
+        'password'  => '',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => 'base_',
+      ],
     ]
 ]);
 
 $container = $app->getContainer();
 
+$capsule = new \Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container['settings']['db']);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$container['db'] = function ($container) use ($capsule){
+  return $capsule;
+};
 
 $container['view'] = function ($container){
     $view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
