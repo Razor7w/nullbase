b0ea4c098467581ce629ff85c0407ee6d6c39e01
Razor7w <seba.carroza@gmail.com>
2019-09-05 05:15:29 -0400

    [NullBase]
    - Se crea el AuthMiddleware para no acceder a ciertas rutas sin estar previamente logeado.

--- app/routes.php
+++ app/routes.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Middleware\AuthMiddleware;
+
 $app->get('/', 'HomeController:index')->setname('home');
 $app->get('/auth/signup', 'AuthController:getSignUp')->setName('auth.signup');
 $app->post('/auth/signup', 'AuthController:postSignUp');
 $app->get('/auth/signin', 'AuthController:getSignIn')->setName('auth.signin');
 $app->post('/auth/signin', 'AuthController:postSignIn');
-$app->get('/auth/signout', 'AuthController:getSignOut')->setName('auth.signout');
-$app->get('/auth/password/change', 'PasswordController:getChangePassword')->setName('auth.password.change');
-$app->post('/auth/password/change', 'PasswordController:postChangePassword');
+
+$app->group('', function(){
+  $this->get('/auth/signout', 'AuthController:getSignOut')->setName('auth.signout');
+  $this->get('/auth/password/change', 'PasswordController:getChangePassword')->setName('auth.password.change');
+  $this->post('/auth/password/change', 'PasswordController:postChangePassword');
+})->add(new AuthMiddleware($container));
